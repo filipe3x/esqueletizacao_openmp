8b383f073734695410ca235dcafeeaf0d3107acc
Filipe Marques <filipemarques@MBP-de-Filipe.lan>
2018-11-25 00:26:48 +0000

    join of ske_v3 and ske.cpp

--- skeletonize.cpp
+++ skeletonize.cpp
@@ -263,3 +263,188 @@ int skeletonize_doublepass_serial (int *I, int W, int H) {
 	return it;
 }
 
+int skeletonize_matrixswap_par (int *I, int W, int H) {
+	int *ch_image = (int*) memalign (32, W * H * sizeof(int)); // which pixels we are going to change
+	//int *cont = (int*) malloc(2 * sizeof(int)); // for checking if we already finished
+
+	/* Neighbors relative coordenates - P0 = total
+						P0 P1 P2 P3 P4 P5 P6 P7 P8 P9 */
+	int PX_index[10] = { 0, 0, 0, 1, 1, 1, 0,-1,-1,-1};
+	int PY_index[10] = { 0, 0,-1,-1, 0, 1, 1, 1, 0,-1};
+
+	int iterations = 0;		// total iterations count
+	//int i, j, k; 			// indexes
+	//int neigh_ant;	// total of neighbors and precious neighbor
+	//int ans;				// total transitions from 0 to 1
+	int *im_read = I, *aux = NULL;
+
+	int cont0 = 1, cont1 = 1;
+
+	// cleaning the padding
+	#pragma omp parallel for
+	for(int u = 0; u < H-1; u++) {
+		ch_image[u*W + 0] = BLACKPIXEL;
+		ch_image[u*W + (W-1)] = BLACKPIXEL;
+	}
+	#pragma omp parallel for
+	for(int u = 0; u < W-1; u++) {
+		ch_image[0 + u] = BLACKPIXEL;
+		ch_image[(H-1)*W + u] = BLACKPIXEL;
+	}
+
+	while((cont0 > 0 || cont1 > 0) || (iterations % 2 != 0)) {
+		cont0 = 0;
+		cont1 = 0;
+		iterations = iterations + 1;
+		//cout << "it=" << iterations << endl;
+
+		#pragma omp parallel for schedule(dynamic) reduction(+:cont0) reduction(+:cont1)
+		for(int i=1; i < H-1; i++) {
+			for(int j=1; j < W-1; j++) {
+				int ans, total, neigh_ant;
+				/* error looking
+				if((im_read[i*W + j] != 1) && (im_read[i*W + j] != 0))
+					cout << im_read[i*W + j] << " em " << i << ", " << j << endl;*/
+				// Only will do something if the point where it is at the moment is a 1;
+				if(im_read[i*W + j] != BLACKPIXEL) {
+					total = 0;
+					ans = 0;
+					neigh_ant = 1;	//redundante
+					// get all neighbors
+					for(int k=2; k < 10; k++) {
+						total += im_read[(i+PY_index[k])*W + (j+PX_index[k])];
+						// previous IF could also be changed for a neighbors[0] = total-1; at end of cycle to take away the 1 from P1.
+						if(neigh_ant - im_read[(i+PY_index[k])*W + (j+PX_index[k])] == -1)
+							ans++;
+						// previous is the same as if(neigh_ant == 0 && neighbors[k] == 1)
+						neigh_ant = im_read[(i+PY_index[k])*W + (j+PX_index[k])];
+					}
+					if(im_read[(i+PY_index[9])*W + (j+PX_index[9])] - im_read[(i+PY_index[2])*W + (j+PX_index[2])] == -1)	// same as neigh_ant - neighbors[2]
+						ans++;
+
+					/*for(int u = 0; u < 10; u++)
+						cout << neighbors[u] << ", ";
+					cout << endl;*/
+
+					if((total >= 2) && (total <= 6) && (ans == 1)) {
+						if((iterations % 2 == 1) && (((1-im_read[(i+PY_index[4])*W + (j+PX_index[4])]) + (1-im_read[(i+PY_index[6])*W + (j+PX_index[6])]) + ((1-im_read[(i+PY_index[2])*W + (j+PX_index[2])]) * (1-im_read[(i+PY_index[8])*W + (j+PX_index[8])]))) >= 1)) {
+							// we remove the pixel
+							ch_image[i*W + j] = BLACKPIXEL;
+							cont0 += 1;
+						} else {
+							if((iterations % 2 == 0) && (((1-im_read[(i+PY_index[2])*W + (j+PX_index[2])]) + (1-im_read[(i+PY_index[8])*W + (j+PX_index[8])]) + ((1-im_read[(i+PY_index[4])*W + (j+PX_index[4])]) * (1-im_read[(i+PY_index[6])*W + (j+PX_index[6])]))) >= 1)) {
+								// we remove the pixel
+								ch_image[i*W + j] = BLACKPIXEL;
+								cont1 += 1;
+							} else {
+								// we don't take it away
+								ch_image[i*W + j] = WHITEPIXEL;
+							}
+						}
+
+					} else {
+						// We don't take the pixel away
+						ch_image[i*W + j] = WHITEPIXEL;
+					}
+				} else {
+					// only copying - sort of
+					ch_image[i*W + j] = BLACKPIXEL;
+				}
+				// error looking
+				//if((ch_image[i*W + j] != 1) && (ch_image[i*W + j] != 0))
+				//	cout << ch_image[i*W + j] << " em " << i << ", " << j << endl;
+			}
+		}
+
+		aux = im_read;
+		im_read = ch_image;
+		ch_image = aux;
+	}
+    return iterations;
+}
+
+int skeletonize_matrixswap_serial (int *I, int W, int H) {
+	int *ch_image = (int*) memalign (32, W * H * sizeof(int)); // which pixels we are going to change
+	//int *cont = (int*) malloc(2 * sizeof(int)); // for checking if we already finished
+
+	/* Neighbors relative coordenates - P0 = total
+		P0 P1 P2 P3 P4 P5 P6 P7 P8 P9 */
+	int PX_index[10] = { 0, 0, 0, 1, 1, 1, 0,-1,-1,-1};
+	int PY_index[10] = { 0, 0,-1,-1, 0, 1, 1, 1, 0,-1};
+
+	int iterations = 0;		// total iterations count
+	//int i, j, k; 			// indexes
+	//int neigh_ant;	// total of neighbors and precious neighbor
+	//int ans;				// total transitions from 0 to 1
+	int *im_read = I, *aux = NULL;
+
+	int cont0 = 1, cont1 = 1;
+
+	// cleaning the padding
+	for(int u = 0; u < H-1; u++) {
+		ch_image[u*W + 0] = BLACKPIXEL;
+		ch_image[u*W + (W-1)] = BLACKPIXEL;
+	}
+	for(int u = 0; u < W-1; u++) {
+		ch_image[0 + u] = BLACKPIXEL;
+		ch_image[(H-1)*W + u] = BLACKPIXEL;
+	}
+
+	while((cont0 > 0 || cont1 > 0) || (iterations % 2 != 0)) {
+		cont0 = 0;
+		cont1 = 0;
+		iterations = iterations + 1;
+
+		for(int i=1; i < H-1; i++) {
+			for(int j=1; j < W-1; j++) {
+				int ans, total, neigh_ant;
+				if(im_read[i*W + j] != BLACKPIXEL) {
+					total = 0;
+					ans = 0;
+					neigh_ant = 1;	//redundante
+					// get all neighbors
+					for(int k=2; k < 10; k++) {
+						total += im_read[(i+PY_index[k])*W + (j+PX_index[k])];
+						// previous IF could also be changed for a neighbors[0] = total-1; at end of cycle to take away the 1 from P1.
+						if(neigh_ant - im_read[(i+PY_index[k])*W + (j+PX_index[k])] == -1)
+							ans++;
+						// previous is the same as if(neigh_ant == 0 && neighbors[k] == 1)
+						neigh_ant = im_read[(i+PY_index[k])*W + (j+PX_index[k])];
+					}
+					if(im_read[(i+PY_index[9])*W + (j+PX_index[9])] - im_read[(i+PY_index[2])*W + (j+PX_index[2])] == -1)	// same as neigh_ant - neighbors[2]
+						ans++;
+
+					if((total >= 2) && (total <= 6) && (ans == 1)) {
+						if((iterations % 2 == 1) && (((1-im_read[(i+PY_index[4])*W + (j+PX_index[4])]) + (1-im_read[(i+PY_index[6])*W + (j+PX_index[6])]) + ((1-im_read[(i+PY_index[2])*W + (j+PX_index[2])]) * (1-im_read[(i+PY_index[8])*W + (j+PX_index[8])]))) >= 1)) {
+							// we remove the pixel
+							ch_image[i*W + j] = BLACKPIXEL;
+							cont0 += 1;
+						} else {
+							if((iterations % 2 == 0) && (((1-im_read[(i+PY_index[2])*W + (j+PX_index[2])]) + (1-im_read[(i+PY_index[8])*W + (j+PX_index[8])]) + ((1-im_read[(i+PY_index[4])*W + (j+PX_index[4])]) * (1-im_read[(i+PY_index[6])*W + (j+PX_index[6])]))) >= 1)) {
+								// we remove the pixel
+								ch_image[i*W + j] = BLACKPIXEL;
+								cont1 += 1;
+							} else {
+								// we don't take it away
+								ch_image[i*W + j] = WHITEPIXEL;
+							}
+						}
+
+					} else {
+						// We don't take the pixel away
+						ch_image[i*W + j] = WHITEPIXEL;
+					}
+				} else {
+					// only copying - sort of
+					ch_image[i*W + j] = BLACKPIXEL;
+				}
+			}
+		}
+
+		aux = im_read;
+		im_read = ch_image;
+		ch_image = aux;
+	}
+    return iterations;
+}
+

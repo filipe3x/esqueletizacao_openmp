0273dcfc9ab284f08102209cb508a0c91a93c333
Pirata <Pirata156@users.noreply.github.com>
2018-12-04 10:59:51 +0000

    alteracao do mpi_inst para mensagem de linhas

--- mpi_inst.cpp
+++ mpi_inst.cpp
@@ -53,7 +53,7 @@ static int cleanup_padding(int *ch_image, int H, int W) {
 	}
 }
 
-int mpi_start(int *I, int W, int H) {
+int mpi_start_backup(int *I, int W, int H) {
 	int n_threads = 1;
 	int tag = 0;
 	int source = 0;
@@ -124,6 +124,97 @@ int mpi_start(int *I, int W, int H) {
 		if(myrank == n_threads-1) { // i'm with the bottom part
 			contOthers = skeletonize_matrixswap_dist(&myimg, &ch_image, W, bottom_block, iteration);
 			MPI_Ssend( myimg + W, block * W + (H % n_threads) * W, MPI_INT, source, tag, MPI_COMM_WORLD);
+			MPI_Recv(myimg , bottom_block * W, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		}
+
+		MPI_Allreduce(&contOthers, &cont0, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+		//if(myrank == 0) { printf("cont0: %d\n", cont0); print_img(I,W,H); printf("\n"); }
+
+		iteration++;
+	}
+
+	//MPI_Abort(MPI_COMM_WORLD, 999);
+
+	return iteration;
+}
+
+int mpi_start(int *I, int W, int H) {
+	int n_threads = 1;
+	int tag = 0;
+	int source = 0;
+	int i = 0;
+	int* myimg, *ch_image, *aux;
+
+	MPI_Status status;
+	MPI_Comm_size(MPI_COMM_WORLD, &n_threads);
+
+	int block = H/n_threads; //number of lines for each process
+	int top_block = block + 1;
+	int bottom_block = 1 + block + (H % n_threads);
+	int middle_block = 1 + block + 1;
+
+	/* scatter image from root into all other processes*/
+
+	if(myrank == 0) {
+		ch_image = (int*) memalign (32, H * W * sizeof(int)); 
+		cleanup_padding(ch_image, H, W);
+
+		mpi_ske_scatter(I,block,middle_block,bottom_block,W,n_threads,tag);
+	} else {
+		// we receive blocks
+		for(i=1; i < n_threads - 1; i++) {
+			if(myrank == i) {
+				myimg = (int*) memalign(0x20, (middle_block * W + 1) * sizeof(int));
+				ch_image = (int*) memalign (32, middle_block * W * sizeof(int)); 
+				cleanup_padding(ch_image, middle_block, W);
+
+				MPI_Recv(myimg , middle_block * W, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			}
+		}
+
+		// last block to be received
+		if(n_threads > 1 && myrank == n_threads - 1) {
+			myimg = (int*) memalign(0x20, bottom_block * W * sizeof(int));
+			ch_image = (int*) memalign (32, bottom_block * W * sizeof(int)); 
+			cleanup_padding(ch_image, bottom_block, W);
+
+			MPI_Recv(myimg , bottom_block * W, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		}
+	}
+
+	// we work on them
+	int cont0 = 1;
+	int contOthers = 1;
+	int iteration = 0;
+	int flag0 = flag1 = 1;
+	while(cont0 > 0) {
+		if(n_threads == 1) { cont0 = skeletonize_matrixswap_dist(&I, &ch_image, W, block, iteration); iteration++; continue; }
+		if(myrank == 0) { // i'm with the top
+			contOthers = skeletonize_matrixswap_dist(&I, &ch_image, W, block+1, iteration);
+
+			/* Gather */
+
+			mpi_ske_gather(I,block,H,W,n_threads,&status);
+
+			/* Scatter */
+
+			mpi_ske_scatter(I,block,middle_block,bottom_block,W,n_threads,tag);
+		}
+
+		if(myrank != 0 && myrank != n_threads-1) { // the virtue is in the middle
+			contOthers = skeletonize_matrixswap_dist(&myimg, &ch_image, W, middle_block, iteration);
+			if(flag0 == 1) MPI_Ssend( &myimg[middle_block-1], W + 1, MPI_INT, source, tag, MPI_COMM_WORLD);
+			if(myimg[0] == 0) flag0 = 0;
+			if(myimg[(middle_block-1)*W] == 0) flag1 = 0;
+
+			if(flag0 == 1) MPI_Recv(myimg, W, MPI_INT, myrank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			if(flag1 == 1) MPI_Recv(myimg[(middle_block - 1)*W] , W, MPI_INT, myrank+1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		}
+
+		if(myrank == n_threads-1) { // i'm with the bottom part
+			contOthers = skeletonize_matrixswap_dist(&myimg, &ch_image, W, bottom_block, iteration);
+			MPI_Ssend( myimg + W, W + 1, MPI_INT, source, tag, MPI_COMM_WORLD);
 			MPI_Recv(myimg , bottom_block * W, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		}
 
